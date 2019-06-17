f5c07f5d51faba8b21b1a853cc498655f8b411f2
Dudsefb <40064775+Dudsefb@users.noreply.github.com>
2019-06-17 18:00:23 -0300

    Create Territorio.cs

---
+++ Guerra/Guerra/Territorio.cs
@@ -0,0 +1,65 @@
+//+src=Map.cs
+//+src=Player.cs
+//+src=Soldado.cs
+//+src=Tropa.cs
+//+src=Starter.cs
+//+src=
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Guerra
+{
+
+    public class Territorio
+    {
+        public readonly string nome;
+        private Player controlador = Starter.PLAYER_NULL;
+        public readonly int id;
+        private int[] conexoes = new int[8];
+        private Tropa[] tropas = new Tropa[100];
+        private int i,t = 0;
+        public Territorio(string nome, int id){
+            this.nome = nome;
+            this.id = id;
+            foreach(int t in conexoes){
+                conexoes[t]=-1;
+            }
+        }
+        public void updateControlador(){
+            controlador = Tropa[0].getController();
+        }
+        public Player getControlador(){
+            return controlador;
+        }
+        public void addConnection(int id){
+            conexoes[i] = id;
+            i++;
+        }
+        public void removeConnection(int id){
+            int c = 0;
+            while(id!=conexoes[c]){
+                if(c<8){
+                    c++;
+                }else{
+                    break;
+                }
+            }
+            conexoes[c] = -1;
+        }
+        public void addSoldado(Player p){
+            tropas[t]= new Soldado(p);
+            t++;
+        }
+        public void removeSoldado(){
+            if(t!=0){
+                tropas[t]= null;
+                t--;
+            }
+        }
+        public int troops(){
+            
+        }
+    }
+}
